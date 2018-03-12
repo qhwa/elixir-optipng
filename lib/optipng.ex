8c1aa8cf6814c2afcc85964e5dbd5f3ea444040f
qhwa <qhwa@163.com>
2018-03-12 16:15:33 +0800

    init commit

---
+++ lib/optipng.ex
@@ -0,0 +1,18 @@
+defmodule Optipng do
+  @moduledoc """
+  Documentation for Optipng.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Optipng.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/optipng_test.exs
@@ -0,0 +1,8 @@
+defmodule OptipngTest do
+  use ExUnit.Case
+  doctest Optipng
+
+  test "greets the world" do
+    assert Optipng.hello() == :world
+  end
+end
